60bf8d3685f737d18c5a53e4a3a1b7868cff931c
Kuzma975 <18575606+Kuzma975@users.noreply.github.com>
2026-01-23 14:34:51 +0200

    update path in config

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
-DATA_DIR = "data"
+basedir = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(basedir, "data")
 STATE_FILE = os.path.join(DATA_DIR, "state.json")
 CALENDARS_FILE = os.path.join(DATA_DIR, "calendars.json")
 CREDENTIALS_FILE = os.path.join(DATA_DIR, "brilliant-forge-176916-0d06b0e3c78e.json")
